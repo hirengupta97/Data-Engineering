ca30e8f433eaffafdc393f5194f523d2667da138
hirengupta97 <hireng@andrew.cmu.edu>
2022-12-08 18:27:29 -0500

    streamlit changes

--- analytics.py
+++ analytics.py
@@ -33,30 +33,34 @@ week = sys.argv[1]
 records = pd.read_sql_query("SELECT collection_week AS Date, "
                             "count(*) AS Hospitals "
                             "FROM hospital_weekly "
-                            # "WHERE collection_week <= %s", (week),
+                            "WHERE collection_week <= %s"
                             "GROUP BY collection_week "
                             "ORDER BY collection_week desc ",
-                            conn)
+                            conn, params=(week,))
 
 # Q2 A table summarizing the number of adult and pediatric beds available
 # this week, the number used, and the number used by patients with COVID,
 # compared to the 4 most recent weeks
 
 beds = pd.read_sql_query("SELECT collection_week, "
-                         "sum(all_adult_hospital_beds_7_day_avg) "
+                         "ROUND(sum(all_adult_hospital_beds_7_day_avg), 1) "
                          "AS adult_beds_available, "
-                         "sum(all_pediatric_inpatient_beds_7_day_avg) "
+                         "ROUND(sum(all_pediatric_inpatient_beds_7_day_avg)) "
                          "AS pediatric_beds_available, "
-                         "sum(all_adult_hospital_inpatient_bed_"
-                         "occupied_7_day_coverage) "
+                         "ROUND(sum(all_adult_hospital_inpatient_bed_"
+                         "occupied_7_day_coverage)) "
                          "AS adult_beds_occupied, "
-                         "sum(all_pediatric_inpatient_bed_occupied_7_day_avg) "
+                         "ROUND(sum(all_pediatric_inpatient_bed_"
+                         "occupied_7_day_avg)) "
                          "AS pediatric_beds_occupied, "
-                         "sum(inpatient_beds_used_covid_7_day_avg) "
+                         "ROUND(sum(inpatient_beds_used_covid_7_day_avg)) "
                          "AS inpatient_beds_used_covid "
                          "FROM hospital_weekly "
-                         "GROUP BY collection_week; ",
-                         conn)
+                         "WHERE collection_week <= %s"
+                         "GROUP BY collection_week "
+                         "ORDER BY collection_week Desc "
+                         "LIMIT 5; ",
+                         conn, params=(week,))
 
 
 # Q3 A graph or table summarizing the fraction of beds currently in use by
@@ -75,28 +79,31 @@ quality = pd.read_sql_query("SELECT overall_quality_rating, beds, "
                             "AS bed_in_use FROM hospital_weekly "
                             "WHERE collection_week = "
                             "(SELECT max(collection_week) "
-                            "FROM hospital_weekly) "
+                            "FROM hospital_weekly "
+                            "WHERE collection_week <= %s) "
                             "GROUP BY hospital_pk) A "
                             "JOIN (SELECT hospital_pk, overall_quality_rating "
                             "FROM hospital_quality) B "
                             "ON A.hospital_pk = B.hospital_pk "
                             "GROUP BY overall_quality_rating "
                             "ORDER BY overall_quality_rating) AS C;",
-                            conn)
+                            conn, params=(week,))
 
 # Q4 A plot of the total number of hospital beds used per week, over all time,
 # split into all cases and COVID cases
 
 covid = pd.read_sql_query("SELECT collection_week, "
-                          "sum(all_adult_hospital_inpatient_bed_occupied_"
-                          "7_day_coverage) + sum(all_pediatric_inpatient_bed_"
-                          "occupied_7_day_avg) AS bed_in_use_all_cases,"
+                          "ROUND(sum(all_adult_hospital_inpatient_bed_"
+                          "occupied_7_day_coverage) + "
+                          "sum(all_pediatric_inpatient_bed_occupied_7_"
+                          "day_avg)) AS bed_in_use_all_cases,"
                           "sum(inpatient_beds_used_covid_7_day_avg) AS "
                           "bed_in_use_covid "
                           "FROM hospital_weekly "
+                          "WHERE collection_week <= %s"
                           "GROUP BY collection_week "
                           "ORDER BY collection_week ASC;",
-                          conn)
+                          conn, params=(week,))
 
 # Q5 A map showing the number of COVID cases by state (the first two digits of
 # a hospital ZIP code is its state)
@@ -111,20 +118,23 @@ map_1 = pd.read_sql_query("SELECT state, sum(covid_cases) "
                           "FROM hospital_weekly "
                           "WHERE collection_week = (SELECT "
                           "max(collection_week) "
-                          "FROM hospital_weekly) "
+                          "FROM hospital_weekly "
+                          "WHERE collection_week <= %s) "
                           "GROUP BY hospital_pk) B "
                           "ON A.hospital_pk = B.hospital_pk "
                           "GROUP BY state; ",
-                          conn)
+                          conn, params=(week,))
 
 # Q6 A table of the states in which the number of cases has increased
 # by the most since last week
 
-covid_2 = pd.read_sql_query("SELECT state, covid_cases_ct, covid_cases_lw "
+covid_2 = pd.read_sql_query("SELECT state, covid_cases_ct, covid_cases_lw, "
+                            "(covid_cases_ct - covid_cases_lw) AS diff "
                             "FROM (SELECT state, sum("
                             "CASE WHEN collection_week = "
                             "(SELECT max(collection_week) "
-                            "FROM hospital_weekly) "
+                            "FROM hospital_weekly "
+                            "WHERE collection_week <= %s) "
                             "THEN inpatient_beds_used_covid_7_day_avg "
                             "END) AS covid_cases_ct, sum("
                             "CASE WHEN collection_week = "
@@ -132,7 +142,8 @@ covid_2 = pd.read_sql_query("SELECT state, covid_cases_ct, covid_cases_lw "
                             "FROM hospital_weekly "
                             "WHERE collection_week < ("
                             "SELECT max(collection_week) "
-                            "FROM hospital_weekly)) "
+                            "FROM hospital_weekly  "
+                            "WHERE collection_week <= %s)) "
                             "THEN inpatient_beds_used_covid_7_day_avg "
                             "END) AS covid_cases_lw "
                             "FROM (SELECT hospital_pk, state "
@@ -142,8 +153,9 @@ covid_2 = pd.read_sql_query("SELECT state, covid_cases_ct, covid_cases_lw "
                             "FROM hospital_weekly) B "
                             "ON A.hospital_pk = B.hospital_pk "
                             "GROUP BY state) AS C "
-                            "WHERE covid_cases_ct > covid_cases_lw;",
-                            conn)
+                            "WHERE covid_cases_ct > covid_cases_lw "
+                            "ORDER BY diff DESC;",
+                            conn, params=(week, week))
 
 # A table of the hospitals (including names and locations) with the
 # largest changes in COVID cases in the last week
@@ -154,7 +166,8 @@ covid_3 = pd.read_sql_query("SELECT hospital_name, address, "
                             "address, hospital_name, sum("
                             "CASE WHEN collection_week = ("
                             "SELECT max(collection_week) "
-                            "FROM hospital_weekly) "
+                            "FROM hospital_weekly "
+                            "WHERE collection_week <= %s) "
                             "THEN inpatient_beds_used_covid_7_day_avg "
                             "END) AS covid_cases_ct, sum("
                             "CASE WHEN collection_week = ("
@@ -162,7 +175,8 @@ covid_3 = pd.read_sql_query("SELECT hospital_name, address, "
                             "FROM hospital_weekly "
                             "WHERE collection_week < "
                             "(SELECT max(collection_week) "
-                            "FROM hospital_weekly)) "
+                            "FROM hospital_weekly "
+                            "WHERE collection_week <= %s)) "
                             "THEN inpatient_beds_used_covid_7_day_avg "
                             "END) AS covid_cases_lw "
                             "FROM (SELECT hospital_pk, hospital_name, address "
@@ -177,7 +191,7 @@ covid_3 = pd.read_sql_query("SELECT hospital_name, address, "
                             "AND covid_cases_lw IS NOT NULL "
                             "ORDER BY diff DESC "
                             "LIMIT 10;",
-                            conn)
+                            conn, params=(week, week))
 
 # Setting the index from 1
 records = records.set_index(records.index + 1)
@@ -201,12 +215,20 @@ st.title("Hospital Operation Analysis Weekly Report %s" % (sys.argv[1]))
 #                       xlabel="Collection Weeks",
 #                       ylabel="Count")
 
-st.header("Summary of Hospitals Added")
+st.subheader("Summary of Hospitals Added")
 
-st.text("We analyze here how many hospitals have been added in the "
-        "respective weeks")
+st.markdown("We analyze here how many hospitals have been added in the"
+            "respective weeks. The system allows the readers to view the"
+            "number of weekly data loaded. In the first column, the"
+            "table consists of the updated week. In the second column, "
+            "the table displays the number of new uploads that have "
+            "been made to the database.")
+
+
+records.columns = ['Collection Week', 'Number of Hospitals']
 
 # Table for question 1
+st.caption("Table 1: Count for Hospitals by week")
 st.table(records)
 # st.write(tabulate(lw_date, headers=lw_date.columns, tablefmt='simple'))
 
@@ -233,7 +255,31 @@ st.table(records)
 # Table for question 2
 # print(tabulate(beds, headers=beds.columns, tablefmt='psql'))
 
-st.table(beds)
+st.subheader("Summary of Bed Operation")
+
+st.markdown("The table allows the readers to view the number of "
+            "beds available each week. Specifically, it illustrates "
+            "the total capacity of each hospital in terms of adult beds, "
+            "pediatric beds, and their occupancy (total number of beds "
+            "occupied by adults or pediatric patients respectively, "
+            "as well as the COVID patients, regardless of the age group)")
+
+st.caption("Table 2: Availability for Hospitals by week")
+
+beds.columns = ['Collection Week',
+                'Adult Beds Available',
+                'Pediatric Beds Available',
+                'Adult Beds Occupied',
+                'Pediatric Beds Occupied',
+                'COVID Beds Used']
+
+st.table(beds.style.format({
+    "Adult Beds Available": "{:.0f}",
+    "Pediatric Beds Available": "{:.0f}",
+    "Adult Beds Occupied": "{:.0f}",
+    "Pediatric Beds Occupied": "{:.0f}",
+    "COVID Beds Used": "{:.0f}"
+    }))
 
 # Annotation for plot of question 2
 # for bar in plot_2.patches:
@@ -250,6 +296,16 @@ st.table(beds)
 
 # st.pyplot(fig2)
 
+st.subheader("Plot for Hospitals on the Basis of Quality")
+
+st.markdown("The plot provides a visual representation of what is "
+            "the quality rating for the occupied beds, providing a "
+            "perspective on high-quality and low-quality beds where "
+            "quality ranges from 1 to 5 and the empty row value for "
+            "rating stands for no rating.")
+
+st.caption("Figure 1: Classification of Hospitals by Ratings")
+
 fig3, ax3 = plt.subplots()
 
 # Plot for question 3
@@ -259,7 +315,7 @@ plot_3 = quality.plot(ax=ax3,
                       y="fraction",
                       xlabel="Ratings",
                       ylabel="Fraction",
-                      title="Rating-wise # of Hospitals")
+                      title="Classification of Hospitals by Ratings")
 
 # Table for question 3
 # print(tabulate(quality, headers=quality.columns, tablefmt='psql'))
@@ -282,6 +338,17 @@ ax3.get_legend().remove()
 
 st.pyplot(fig3)
 
+
+st.subheader("Plot for Hospital Beds Occupied on the Basis of Type of Case")
+
+st.markdown("The plot provides a visual representation of what is the "
+            "bifurcation for the occupied beds on the basis of what type of "
+            "case it is - COVID or otherwise, informing and indicating the "
+            "count for other cases compared to COVID.")
+
+st.caption("Figure 2: Classification of Hospitals by Type of Case")
+
+
 fig4, ax4 = plt.subplots()
 
 
@@ -289,8 +356,9 @@ fig4, ax4 = plt.subplots()
 plot_4 = covid.plot(ax=ax4,
                     kind="bar",
                     x="collection_week",
-                    xlabel="Total",
-                    ylabel="Beds in Use")
+                    xlabel="Week",
+                    ylabel="Beds in Use",
+                    title="Classification of Hospitals by Type of Case")
 
 # Table for question 4
 # print(tabulate(covid, headers=covid.columns, tablefmt='psql'))
@@ -309,6 +377,22 @@ plt.legend(loc='best')
 # plt.show()
 
 ax4.get_legend().remove()
+
+st.pyplot(fig4)
+# plt.show()
+
+st.subheader("Map Visualization of COVID Cases by State")
+
+st.markdown("This is a map visualization of the number of COVID "
+            "patients registered to hospitals, aggregated by the States "
+            "where the hospitals are located. The number of total COVID "
+            "cases are color coded - the States with lower number of "
+            "patients will be colored with yellow (lighter color, 0~500), "
+            "and the States with higher number of patients will be colored "
+            "with indigo (darker color, 3000+ patients per State).")
+
+st.caption("Figure 3: Interactive Map for COVID Cases by State")
+
 
 # Plot for question 5
 plot_5 = px.choropleth(map_1,
@@ -329,12 +413,41 @@ st.plotly_chart(plot_5)
 # Table for queation 6
 # print(tabulate(covid_2, headers=covid_2.columns, tablefmt='psql'))
 
-st.table(covid_2)
+
+st.subheader("States with the Highest Increase in the Number of COVID Cases")
+
+st.markdown("This table displays the number of increased patient occupancy"
+            " for COVID cases per each State. Specifically, the number of "
+            "patients would be ordered in descending order, showing the State "
+            "with the highest number of increase in the COVID patients "
+            "compared to the last week’s data.")
+
+st.caption("Table 3: Highest Number of COVID Cases in States")
+
+covid_2.columns = ["State", "COVID Cases of Current Week",
+                   "COVID cases of Last Week", "Difference"]
+
+covid_3.columns = ["Hospital Name", "Address", "Difference"]
+
+st.table(covid_2.style.format({
+    "COVID Cases of Current Week": "{:.0f}",
+    "COVID cases of Last Week": "{:.0f}",
+    "Difference": "{:.0f}"
+    }))
+
+st.subheader("Hospital with the Highest Increase in the Number of COVID Cases")
+
+st.markdown("This table displays the number of increased patient occupancy "
+            "for COVID cases per each hospital. Specifically, the number of "
+            "patients would be ordered in descending order, showing the "
+            "hospital with the highest number of increase in the COVID "
+            "patients compared to the last week’s data.")
+
+st.caption("Table 4: Highest Number of COVID Cases in Hospitals")
 
 # Table for queation 7
 # print(tabulate(covid_3, headers=covid_3.columns, tablefmt='psql'))
 
-st.table(covid_3)
-
-st.pyplot(fig4)
-# plt.show()
+st.table(covid_3.style.format({
+    "Difference": "{:.0f}"
+    }))
