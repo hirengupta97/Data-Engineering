f399935d785147a0553d03d76acaf9fb1aa13490
hirengupta97 <hireng@andrew.cmu.edu>
2022-12-08 02:19:42 -0500

    streamlit changes

--- analytics.py
+++ analytics.py
@@ -4,10 +4,11 @@
 import psycopg
 import pandas as pd
 import matplotlib.pyplot as plt
-from tabulate import tabulate
+# from tabulate import tabulate
 import warnings
 import plotly.express as px
 import streamlit as st
+import sys
 
 
 warnings.filterwarnings("ignore")
@@ -16,6 +17,8 @@ conn = psycopg.connect(
     user="ruilinw", password="Eip8oosei"
 )
 cur = conn.cursor()
+
+week = sys.argv[1]
 
 # d = pd.read_sql_query("SELECT collection_week, count(*)"
 #                       "FROM hospital_weekly "
@@ -27,8 +30,10 @@ cur = conn.cursor()
 # Q1 A summary of how many hospital records were loaded in the most recent
 # week, and how that compares to previous weeks.
 
-records = pd.read_sql_query("SELECT collection_week, count(*) AS count "
+records = pd.read_sql_query("SELECT collection_week AS Date, "
+                            "count(*) AS Hospitals "
                             "FROM hospital_weekly "
+                            # "WHERE collection_week <= %s", (week),
                             "GROUP BY collection_week "
                             "ORDER BY collection_week desc ",
                             conn)
@@ -58,22 +63,25 @@ beds = pd.read_sql_query("SELECT collection_week, "
 # hospital quality rating, so we can compare high-quality
 # and low-quality hospitals
 
-quality = pd.read_sql_query("SELECT overall_quality_rating, "
-                            "sum(bed_in_use) AS bed_in_use "
+quality = pd.read_sql_query("SELECT overall_quality_rating, beds, "
+                            "ROUND(beds/sum(beds) OVER (),2) AS fraction "
+                            "FROM (SELECT overall_quality_rating, "
+                            "sum(bed_in_use) AS beds "
                             "FROM (SELECT hospital_pk, "
-                            "sum(all_adult_hospital_inpatient_bed_occupied_"
-                            "7_day_coverage) + sum(all_pediatric_inpatient_"
-                            "bed_occupied_7_day_avg) AS bed_in_use "
-                            "FROM hospital_weekly "
+                            "sum(all_adult_hospital_inpatient_"
+                            "bed_occupied_7_day_coverage) + "
+                            "sum(all_pediatric_inpatient_"
+                            "bed_occupied_7_day_avg) "
+                            "AS bed_in_use FROM hospital_weekly "
                             "WHERE collection_week = "
                             "(SELECT max(collection_week) "
                             "FROM hospital_weekly) "
                             "GROUP BY hospital_pk) A "
                             "JOIN (SELECT hospital_pk, overall_quality_rating "
-                            "FROM hospital_quality) B ON A.hospital_pk = "
-                            "B.hospital_pk "
+                            "FROM hospital_quality) B "
+                            "ON A.hospital_pk = B.hospital_pk "
                             "GROUP BY overall_quality_rating "
-                            "ORDER BY overall_quality_rating ASC;",
+                            "ORDER BY overall_quality_rating) AS C;",
                             conn)
 
 # Q4 A plot of the total number of hospital beds used per week, over all time,
@@ -181,64 +189,82 @@ covid_2 = covid_2.set_index(covid_2.index + 1)
 covid_3 = covid_3.set_index(covid_3.index + 1)
 
 # Implement streamlit
-st.title("Hospital Operation Analysis Weekly Report")
+st.title("Hospital Operation Analysis Weekly Report %s" % (sys.argv[1]))
 
-fig, ax = plt.subplots()
+# fig1, ax1 = plt.subplots()
 
 # Plot for question 1
-plot_1 = records.plot(ax=ax,
-                      kind="bar",
-                      x="collection_week",
-                      y="count",
-                      xlabel="Collection Weeks",
-                      ylabel="Count")
+# plot_1 = records.plot(ax=ax1,
+#                       kind="bar",
+#                       x="collection_week",
+#                       y="count",
+#                       xlabel="Collection Weeks",
+#                       ylabel="Count")
+
+st.header("Summary of Hospitals Added")
+
+st.text("We analyze here how many hospitals have been added in the "
+        "respective weeks")
 
 # Table for question 1
 st.table(records)
 # st.write(tabulate(lw_date, headers=lw_date.columns, tablefmt='simple'))
 
 # Annotation for plot of question 1
-for bar in plot_1.patches:
-    plt.annotate(format(bar.get_height()),
-                 (bar.get_x() + bar.get_width() / 2,
-                 bar.get_height()), ha='center', va='center',
-                 size=10, xytext=(0, 8),
-                 textcoords='offset points')
+# for bar in plot_1.patches:
+#     plt.annotate(format(bar.get_height()),
+#                  (bar.get_x() + bar.get_width() / 2,
+#                  bar.get_height()), ha='center', va='center',
+#                  size=10, xytext=(0, 8),
+#                  textcoords='offset points')
 
-ax.get_legend().remove()
+# ax1.get_legend().remove()
 
+# st.pyplot(fig1)
 
-st.pyplot(fig)
-# plt.show()
+# fig2, ax2 = plt.subplots()
 
-# Plot for question 2
-plot_2 = beds.plot(kind="bar",
-                   x="collection_week",
-                   xlabel="Beds Status",
-                   ylabel="Total")
+# # Plot for question 2
+# plot_2 = beds.plot(ax=ax2,
+#                    kind="bar",
+#                    x="collection_week",
+#                    xlabel="Beds Status",
+#                    ylabel="Total")
 # Table for question 2
-print(tabulate(beds, headers=beds.columns, tablefmt='psql'))
+# print(tabulate(beds, headers=beds.columns, tablefmt='psql'))
+
+st.table(beds)
 
 # Annotation for plot of question 2
-for bar in plot_2.patches:
-    plt.annotate(format(bar.get_height()),
-                 (bar.get_x() + bar.get_width() / 2,
-                 bar.get_height()), ha='center', va='center',
-                 size=10, xytext=(0, 8),
-                 textcoords='offset points')
+# for bar in plot_2.patches:
+#     plt.annotate(format(bar.get_height()),
+#                  (bar.get_x() + bar.get_width() / 2,
+#                  bar.get_height()), ha='center', va='center',
+#                  size=10, xytext=(0, 8),
+#                  textcoords='offset points')
 
-plt.legend(loc='best', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
-plt.show()
+# plt.legend(loc='best', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
+# # plt.show()
+
+# ax2.get_legend().remove()
+
+# st.pyplot(fig2)
+
+fig3, ax3 = plt.subplots()
 
 # Plot for question 3
-plot_3 = quality.plot(kind="bar",
+plot_3 = quality.plot(ax=ax3,
+                      kind="bar",
                       x="overall_quality_rating",
+                      y="fraction",
                       xlabel="Ratings",
-                      ylabel="Beds in Use")
+                      ylabel="Fraction",
+                      title="Rating-wise # of Hospitals")
 
 # Table for question 3
-print(tabulate(quality, headers=quality.columns,
-               tablefmt='psql'))
+# print(tabulate(quality, headers=quality.columns, tablefmt='psql'))
+
+# st.table(quality)
 
 # Annotation for plot of question 3
 for bar in plot_3.patches:
@@ -250,17 +276,26 @@ for bar in plot_3.patches:
 
 plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
            ncol=3, fancybox=True, shadow=True)
-plt.show()
+# plt.show()
+
+ax3.get_legend().remove()
+
+st.pyplot(fig3)
+
+fig4, ax4 = plt.subplots()
+
 
 # Plot for question 4
-plot_4 = covid.plot(kind="bar",
+plot_4 = covid.plot(ax=ax4,
+                    kind="bar",
                     x="collection_week",
                     xlabel="Total",
                     ylabel="Beds in Use")
 
 # Table for question 4
-print(tabulate(covid, headers=covid.columns,
-               tablefmt='psql'))
+# print(tabulate(covid, headers=covid.columns, tablefmt='psql'))
+
+# st.table(covid)
 
 # Annotation for plot of question 4
 for bar in plot_4.patches:
@@ -271,7 +306,9 @@ for bar in plot_4.patches:
                  textcoords='offset points')
 
 plt.legend(loc='best')
-plt.show()
+# plt.show()
+
+ax4.get_legend().remove()
 
 # Plot for question 5
 plot_5 = px.choropleth(map_1,
@@ -281,13 +318,23 @@ plot_5 = px.choropleth(map_1,
                        color='total_covid_cases',
                        color_continuous_scale="Viridis_r",
                        )
-plot_5.show()
+# plot_5.show()
+st.plotly_chart(plot_5)
 
 # Table for queation 5
-print(tabulate(map_1, headers=map_1.columns, tablefmt='psql'))
+# print(tabulate(map_1, headers=map_1.columns, tablefmt='psql'))
+
+# st.table(map_1)
 
 # Table for queation 6
-print(tabulate(covid_2, headers=covid_2.columns, tablefmt='psql'))
+# print(tabulate(covid_2, headers=covid_2.columns, tablefmt='psql'))
+
+st.table(covid_2)
 
 # Table for queation 7
-print(tabulate(covid_3, headers=covid_3.columns, tablefmt='psql'))
+# print(tabulate(covid_3, headers=covid_3.columns, tablefmt='psql'))
+
+st.table(covid_3)
+
+st.pyplot(fig4)
+# plt.show()
